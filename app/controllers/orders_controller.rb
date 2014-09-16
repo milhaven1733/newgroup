33111fad35ecfe5f70703a9a28283ed1c9a1f09f
AmenZhou <chou.amen@gmail.com>
2014-09-16 21:16:21 +0000

    Fixed #32 show error message on page when left tickets are not enough

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,11 +6,16 @@ class OrdersController < ApplicationController
                                 .merge(user: current_user,
                                        price: @ticket.price_when(order_params[:count]),
                                        shipping: @ticket.shipping))
-    if @order.pay_by(current_user)
+    result = @order.pay_by(current_user)
+
+    if result == Order::PayByResult[0]
       redirect_to [:mine, @order]
-    else
+    elsif result == Order::PayByResult[1]
       redirect_to mine_wallet_path, error: 'Please recharge first'
+    elsif result == Order::PayByResult[2]
+      flash[:error] = "Not enough tickets"
     end
+    redirect_to @ticket
   end
 
   private
--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,8 @@ class Order < ActiveRecord::Base
   validates :user_id, :ticket_id, :price_in_cents, :count, presence: true
   validates :price_in_cents, numericality: { greater_than: 0 }
   validates :count, numericality: { greater_than: 4 }
-
+  
+  PayByResult = [:sucess, :not_enough_money, :not_enough_tickets]
   def amount
     shipping_fee_included? ? price * count + shipping : price * count
   end
@@ -17,10 +18,10 @@ class Order < ActiveRecord::Base
   def shipping_fee_included?
     !will_call
   end
-  
+
   def pay_by(user)
-    return false unless user.afford?(amount)
-    return false unless ticket.ticket_enough?(count)
+    return PayByResult[1] unless user.afford?(amount)
+    return PayByResult[2] unless ticket.ticket_enough?(count)
     Order.transaction do
       wallet = Wallet.find_by! user_id: user_id
       wallet.update! balance: user.balance - amount
@@ -28,5 +29,6 @@ class Order < ActiveRecord::Base
       self.status = :paid
       self.save!
     end
+    PayByResult[0]
   end
 end
