8dcae208f76fa8476cd8db82b91bca43a5f79e41
amenzhou <chou.amen@gmail.com>
2014-11-14 23:09:15 +0000

    #52 profile page under user account

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,17 +1,19 @@
 class ProfileController < ApplicationController
-  before_action :set_user, only: [:merchant]
+  before_action :set_user
 
   def merchant
+    return unless @user.merchant?
     @top_tickets = Ticket.top_deals
   end
 
   def user
+    return if @user.merchant?
     #TODO: what is the defination of favorite shows? 
     @favorite_shows = Ticket.last(6)
     #TODO: What is the defination of favorite venues
     @favorite_venues = Ticket.last(6)
   end
-  
+
   private
 
   def set_user
