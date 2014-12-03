ad92e0f3e73c0b1f08f5f153ff1b71c39ef0ee39
AmenZhou <chou.amen@gmail.com>
2014-12-03 23:45:06 +0000

    add test code for price tier

--- app/models/group_price.rb
+++ app/models/group_price.rb
@@ -4,7 +4,7 @@ class GroupPrice < ActiveRecord::Base
   belongs_to :ticket
 
   validates :range_from, :price_in_cents, presence: true, numericality: true
-  
+
   def count_range
     "#{ range_from } to #{ range_to }"
   end
