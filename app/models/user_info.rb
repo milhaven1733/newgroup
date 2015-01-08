f2f9cdde39620aee6a7f118418cfdc06c86b008a
AmenZhou <chou.amen@gmail.com>
2015-01-08 22:59:56 +0000

    #92 set is_student value when profile updated

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -3,4 +3,10 @@ class UserInfo < ActiveRecord::Base
   belongs_to :user
   has_one :address, as: :addressable, class_name: 'Adress'
   accepts_nested_attributes_for :address
+
+  before_create :set_student
+
+  def set_student
+    self.is_student = true if is_student.nil? and user.student_email_valid?
+  end
 end
