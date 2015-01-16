bf5f1225a5e647752391fbc73b84761613634788
caok <caok1231@gmail.com>
2015-01-16 11:52:40 +0800

    fix the upload bug for producton

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :bundle_without,  [:development, :test]
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml', 'config/secrets.yml', 'config/config.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
  
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
