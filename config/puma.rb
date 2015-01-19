3be5cf8f35649d39ae232bdd771da8e6d73540f1
caok <caok1231@gmail.com>
2015-01-19 09:53:41 +0800

    add log for puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,14 @@
-user = "brtr"                                                                                                                                                                    
-application = "group"          
+user = "brtr"
+application = "group"
+path = "/home/#{user}/apps/#{application}/shared"
  
 port 5000
-threads 0, 4                   
+threads 0, 4
 workers 1
-      
-pidfile "/home/#{user}/apps/#{application}/shared/tmp/pids/puma.#{application}.pid"
-      
+
+pidfile "#{path}/tmp/pids/puma.#{application}.pid"
+stdout_redirect "#{path}/log/puma.stdout", "#{path}/log/puma.stderr", true
+
 preload_app!
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
