0c1b9b348c249268c6c0fcc53340f393ebb56123
AmenZhou <chou.amen@gmail.com>
2015-01-19 21:38:08 +0000

    font show problem under production env

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,17 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( form_validate.js slider.js )
 Rails.application.config.assets.precompile += %w( *.jpg *.png *.gif *.jpeg )
 Rails.application.config.assets.precompile += %w( application_mobile.css application_mobile.js )
+Rails.application.config.assets.precompile << Proc.new do |path|
+   if path =~ /\.(css|js|scss|coffee)\z/
+     full_path = Rails.application.assets.resolve(path).to_path
+     app_assets_path = Rails.root.join('app', 'assets').to_path
+     vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
+     if full_path.starts_with?(app_assets_path) || full_path.starts_with?(vendor_assets_path)
+       true
+     else
+       false
+     end
+   else
+     false
+   end
+ end
